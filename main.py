0ccfe75fd25eb1aad8c6f7bd6396e94eb50d7f0d
k-srivastava <srivastavak1223@gmail.com>
2025-01-24 09:37:16 +0530

    Added user routing to the main app.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 
-from router import pass_
+from router import pass_, user
 
 app = FastAPI()
 app.include_router(pass_.router)
+app.include_router(user.router)
 
 
 @app.get("/")
