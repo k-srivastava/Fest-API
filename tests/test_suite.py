1ec471c4a7b179e3abef1682d840dd5752ac100d
k-srivastava <srivastavak1223@gmail.com>
2025-02-13 15:21:01 +0530

    Fixed test suite to run correctly.

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -2,13 +2,13 @@ import os
 import sys
 import unittest
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from tests.associations import AssociationTest
 from tests.event import EventTest
 from tests.pass_ import PassTest
 from tests.team import TeamTest
 from tests.user import UserTest
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 def create_suite() -> unittest.TestSuite:
